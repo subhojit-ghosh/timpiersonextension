5a2156a2a9d90e18919150c4845da95aa63b77de
Subhojit Ghosh <subhojitghosh064@gmail.com>
2022-04-24 20:14:35 +0530

    refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -4,26 +4,18 @@ export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "timpiersonextension.openfiles",
     async () => {
-      const jsFiles = await vscode.workspace.findFiles(
-        "**/*.js",
+      const allFiles = await vscode.workspace.findFiles(
+        "**/*.{js,ts,yaml}",
         "**/node_modules/**"
       );
-      const tsFiles = await vscode.workspace.findFiles(
-        "**/*.ts",
-        "**/node_modules/**"
-      );
-      const yamlFiles = await vscode.workspace.findFiles(
-        "**/*.yaml",
-        "**/node_modules/**"
-      );
-
-      const allFiles = [...jsFiles, ...tsFiles, ...yamlFiles];
       for (const file of allFiles) {
-        const openPath = vscode.Uri.parse("file://" + file.path);
-        const doc = await vscode.workspace.openTextDocument(openPath);
-        await vscode.window.showTextDocument(doc, {
-          preview: false,
-        });
+        try {
+          const openPath = vscode.Uri.parse("file://" + file.path);
+          const doc = await vscode.workspace.openTextDocument(openPath);
+          await vscode.window.showTextDocument(doc, {
+            preview: false,
+          });
+        } catch (error) {}
       }
     }
   );
